dec23af0273510145a872c18d4852983bc14290b
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-11-03 23:26:55 +0500

    update

---
+++ app.js
@@ -0,0 +1,17 @@
+// function myFunction() {
+//   console.log(this.name);
+// }
+
+// const obj = { name: "Yako" };
+
+// myFunction.call(obj);
+
+const array = [1, 2, 3, 4];
+
+array.forEach((i) => console.log(i * 2));
+
+console.log(array);
+
+// const array = [1, 2, 3, 4];
+
+// array.map((i) => console.log(i));
